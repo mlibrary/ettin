56f76736a7def77f71e122fa1a1beeea5068da8c
Bryan Hockey <bhock@umich.edu>
2018-02-02 23:16:18 -0600

    indentation

--- lib/ettin/yaml_source.rb
+++ lib/ettin/yaml_source.rb
@@ -13,7 +13,8 @@ module Ettin
         YAML.load(ERB.new(File.read(path)).result) || {}
       rescue Psych::SyntaxError => e
         raise "YAML syntax error occurred while parsing #{@path}. " \
-          "Please note that YAML must be consistently indented using spaces. Tabs are not allowed. " \
+          "Please note that YAML must be consistently indented using " \
+          "spaces. Tabs are not allowed. " \
           "Error: #{e.message}"
       end
     end
