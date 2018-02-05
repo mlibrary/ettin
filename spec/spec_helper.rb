e13174503f3fbe160a3fe81ceeab49e16dee550a
Bryan Hockey <bhock@umich.edu>
2018-02-05 02:03:38 -0600

    Add test coverage filters

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 require "simplecov"
-SimpleCov.start
+SimpleCov.start do
+  add_filter "/spec/"
+  add_filter "/.bundle/"
+end
 
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
