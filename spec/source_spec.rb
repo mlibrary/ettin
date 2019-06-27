c263d4067c665549fb751ff6fd8d0a4e6cb99321
Bryan Hockey <bhock@umich.edu>
2019-06-27 15:18:46 -0500

    Appease rubocop

--- spec/source_spec.rb
+++ spec/source_spec.rb
@@ -1,10 +1,11 @@
+# frozen_string_literal: true
+
 require "ettin/source"
 
 RSpec.describe Ettin::Source do
   describe "#load" do
     it "is not implemented" do
-      expect {described_class.new.load}.to raise_error(NotImplementedError)
+      expect { described_class.new.load }.to raise_error(NotImplementedError)
     end
   end
-
 end
